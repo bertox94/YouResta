01d1d8e3cee77ad9745aa81d1696a947570fe007
bertox94 <alberto1.tiraboschi@mail.polimi.it>
2019-12-14 16:05:03 +0100

    OK

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:youresta/screens/wrapper.dart';
-import 'package:youresta/services/auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:youresta/models/user.dart';
+import 'json_array.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,11 +7,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<User>.value(
-      value: AuthService().user,
-      child: MaterialApp(
-        home: Wrapper(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
+      home: StreamBuilderFireStore(),
     );
   }
 }
